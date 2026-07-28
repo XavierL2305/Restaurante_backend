6b4a7776dc205d1c1cb6e3909e7ed419d631bf16
RamssesRM <isram0717@gmail.com>
2026-07-28 18:48:06 -0400

    arreglo de logueo y sesiones

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = [
     'localhost', 
     '127.0.0.1', 
     '10.0.2.2', 
+    '192.168.0.111',
     '192.168.1.105',
     '192.168.1.203'
 ] # Pon tu IPv4 real también por si acaso
